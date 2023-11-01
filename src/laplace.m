857adbf5924b23a8afeb9dc44075effcef5b253e
Haruray <syafiqfaray2@gmail.com>
2023-11-01 19:08:00 +0700

    upload data

--- src/laplace.m
+++ src/laplace.m
@@ -7,5 +7,4 @@ function result = laplace(img)
     img = rgb2gray(img);
     R = [1 1 1;1 -8 1; 1 1 1];
     result = uint8(convn(double(img), double(R), 'same'));
-    %figure, imshow(result);
 end
